540dfc1b110e6f8ee259d3f654884478a703d7a3
ZiZIGY <alexandrkraft7@gmail.com>
2024-10-03 17:18:15 +0300

    added structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,6 @@
 <script lang="ts"></script>
 <template>
-  <div>
-    <input
-      v-for="val in 10"
-      id="id"
-      :key="val"
-      type="text"
-      name="base"
-      class="askljdkolas"
-    />
-  </div>
+  <div> </div>
 </template>
 
 <style></style>
